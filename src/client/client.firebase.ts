42c5224f1a6b3cdba34be2568dea834952f74b41
Timi Tayo <timilehin.ty@gmail.com>
2024-02-01 17:02:07 +0100

    Presence service

--- src/client/client.firebase.ts
+++ src/client/client.firebase.ts
@@ -1,15 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
 import { getAuth, signInWithCustomToken } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getDatabase,
+  onDisconnect,
+  onValue,
+  ref,
+  serverTimestamp,
+  set,
+} from "firebase/database";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA8Txu3DhuE6KwWtA84NKiCE-25DErLLU0",
   authDomain: "spinder-e2ede.firebaseapp.com",
+  databaseURL:
+    "https://spinder-e2ede-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "spinder-e2ede",
   storageBucket: "spinder-e2ede.appspot.com",
   messagingSenderId: "994462371760",
@@ -19,8 +23,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
 const auth = getAuth(app);
+const database = getDatabase(app);
 
 async function firebaseSignInWithCustomToken(token: string): Promise<void> {
   try {
@@ -29,6 +33,7 @@ async function firebaseSignInWithCustomToken(token: string): Promise<void> {
     console.log(
       `Firebase authenticated user with ID: ${user.uid}, Name: ${user.displayName}`
     );
+    attachPresenceService();
     return;
   } catch (error) {
     console.error(error);
@@ -39,18 +44,50 @@ async function firebaseSignInWithCustomToken(token: string): Promise<void> {
 }
 
 async function getFirebaseIdToken(): Promise<string> {
+  if (!auth.currentUser) {
+    throw new Error("Failed to get firebase id token. User is not logged in.");
+  }
+
   try {
     console.log("Firebase trying to get id token...");
-    const idToken = (await auth.currentUser?.getIdToken(true)) || null;
-    if (idToken) {
-      return idToken;
-    } else {
-      throw new Error("Failed to get firebase id token.");
-    }
+    const idToken = await auth.currentUser.getIdToken(true);
+    return idToken;
   } catch (error) {
     console.error(error);
     throw new Error("Failed to get firebase id token.");
   }
+}
+
+function attachPresenceService() {
+  if (!auth.currentUser) {
+    throw new Error(
+      "Failed to attach presence service. User is not logged in."
+    );
+  }
+
+  var uid = auth.currentUser.uid;
+  var userStatusDatabaseRef = ref(database, "/status/" + uid);
+
+  var isOfflineForDatabase = {
+    state: "offline",
+    last_changed: serverTimestamp(),
+  };
+  var isOnlineForDatabase = {
+    state: "online",
+    last_changed: serverTimestamp(),
+  };
+
+  const isConnectedRef = ref(database, ".info/connected");
+  onValue(isConnectedRef, function (snapshot) {
+    if (snapshot.val() == false) {
+      return;
+    }
+    onDisconnect(userStatusDatabaseRef)
+      .set(isOfflineForDatabase)
+      .then(function () {
+        set(userStatusDatabaseRef, isOnlineForDatabase);
+      });
+  });
 }
 
 export { firebaseSignInWithCustomToken, getFirebaseIdToken };
