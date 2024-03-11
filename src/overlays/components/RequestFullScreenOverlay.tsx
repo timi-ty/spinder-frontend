555aa0cca0954f041992fcb605608d596c460f93
Timi Tayo <timilehin.ty@gmail.com>
2024-03-11 14:09:49 +0100

    Ignore touch events while requesting full screen

--- src/overlays/components/RequestFullScreenOverlay.tsx
+++ src/overlays/components/RequestFullScreenOverlay.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect } from "react";
 import "../styles/RequestFullScreenOverlay.scss";
 
 function swithToFullScreen() {
@@ -9,6 +10,38 @@ function swithToFullScreen() {
 }
 
 function RequestFullScreenOverlay() {
+  //Block all window level listeners (the app should not be interactive while waiting for full screen mode). This works because we assume this component is always mounted before other interactive components.
+  const blockAllMouse = useCallback((ev: MouseEvent) => {
+    ev.stopImmediatePropagation();
+  }, []);
+  const blockAllTouch = useCallback((ev: TouchEvent) => {
+    ev.stopImmediatePropagation();
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("mousedown", blockAllMouse);
+    window.addEventListener("mouseup", blockAllMouse);
+    window.addEventListener("mousemove", blockAllMouse);
+
+    return () => {
+      window.removeEventListener("mousedown", blockAllMouse);
+      window.removeEventListener("mouseup", blockAllMouse);
+      window.removeEventListener("mousemove", blockAllMouse);
+    };
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("touchstart", blockAllTouch);
+    window.addEventListener("touchmove", blockAllTouch);
+    window.addEventListener("touchend", blockAllTouch);
+
+    return () => {
+      window.removeEventListener("touchstart", blockAllTouch);
+      window.removeEventListener("touchmove", blockAllTouch);
+      window.removeEventListener("touchend", blockAllTouch);
+    };
+  }, []);
+
   return (
     <div className="request-full-screen" onClick={swithToFullScreen}>
       <div className="message">
